408173ff9b0d8fd83f1b8cba2f302707dc755e87
ryamada1015 <60910478+ryamada1015@users.noreply.github.com>
2021-07-17 21:45:05 -0700

    Add files via upload

---
+++ Assignment2/MyFile.java
@@ -0,0 +1,54 @@
+package edu.sjsu.assignment2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class MyFile {
+
+	public static void main(String[] args) {
+		try {
+			writePrimes("file1.txt", "file2.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found");
+		}
+	}
+	
+	public static void writePrimes(String file1, String file2) throws FileNotFoundException {
+		File inputFile = new File(file1);
+		PrintWriter outputFile = new PrintWriter(file2);
+		
+		Scanner reader = new Scanner(inputFile);
+			
+		reader.useDelimiter(",");
+		
+		while(reader.hasNext()) {
+			String input = reader.next();
+			if(!isInt(input)) continue;
+			int num = Integer.parseInt(input);
+			if(num <= 1) continue;
+			int i;
+			for(i = 2; i < num; i++) {
+				if(num%i == 0)
+					break;
+			}
+			if(i == num)
+				outputFile.println(num);
+			}
+				
+		reader.close();
+		outputFile.close();
+		
+		
+	}
+	
+	private static boolean isInt(String str) {
+		for(int i = 0; i < str.length(); i++) {
+			if(!Character.isDigit(str.charAt(i))) 
+				return false;
+		}
+		return true;
+	}
+
+}
---
+++ Assignment2/MyRecursion.java
@@ -0,0 +1,24 @@
+package edu.sjsu.assignment2;
+
+public class MyRecursion {
+
+	public static void main(String[] args) {
+		System.out.println(indexOf("mississippi", "sip"));
+		
+	}
+	
+	public static int indexOf(String s1, String s2) {
+		return findMatch(s1, s2, 0);
+	}
+	
+	private static int findMatch(String s1, String s2, int index) {
+		if(index+s2.length() >= s1.length()) {
+			return -1;
+		}
+		else if(s1.substring(index, index+s2.length()).equals(s2)) {
+			return index;			
+		}
+		return findMatch(s1, s2, index+1);
+
+}
+}
