fb019c28bf3e3d7a6982947c10ee439962cf26c0
ryamada1015 <60910478+ryamada1015@users.noreply.github.com>
2021-07-17 21:29:29 -0700

    Add files via upload

--- Assignment1/Card.java
+++ Assignment1/Card.java
@@ -3,7 +3,7 @@ package edu.sjsu.assignment1;
 public class Card {
 
 	public static void main(String[] args) {
-		Card card = new Card("10H");
+		Card card = new Card("SK");
 		String str = card.getDescription();
 		System.out.println(str);
 	}
@@ -41,31 +41,37 @@ public class Card {
 				}
 			}
 			else if(!Character.isDigit(ch)) {
-				switch(ch) {
-				case 'A': number = "Ace";
-				break;
-				case 'J': number = "Jack";
-				break;
-				case 'Q': number = "Queen";
-				break;
-				case 'K': number = "King";
-				break;
+				if(ch == 'A' || ch == 'J' || ch == 'Q' || ch == 'k') {
+					switch(ch) {
+					case 'A': number = "Ace";
+					break;
+					case 'J': number = "Jack";
+					break;
+					case 'Q': number = "Queen";
+					break;
+					case 'K': number = "King";
+					break;
+					}
 				}
+				else return "Unknown";
 			}
 			else {
 				return "Unknown";
 			}
 			char ch2 = notation.charAt(1);
-			switch(ch2) {
-			case 'S': suit = "Spades";
-			break;
-			case 'C': suit = "Clubs";
-			break;
-			case 'H': suit = "Hearts";
-			break;
-			case 'D': suit = "Diamonds";
-			break;
+			if(ch2 == 'S' || ch2 == 'C' || ch2 == 'H' || ch2 == 'D') {
+				switch(ch2) {
+				case 'S': suit = "Spades";
+				break;
+				case 'C': suit = "Clubs";
+				break;
+				case 'H': suit = "Hearts";
+				break;
+				case 'D': suit = "Diamonds";
+				break;
+				}
 			}
+			else return "Unknown";
 		}
 		else if(notation.length() == 3) {
 			if(notation.charAt(0) == '1' && notation.charAt(1) == '0') {
@@ -76,16 +82,19 @@ public class Card {
 			}
 			
 			char ch3 = notation.charAt(2);
-			switch(ch3) {
-			case 'S': suit = "Spades";
-			break;
-			case 'C': suit = "Clubs";
-			break;
-			case 'H': suit = "Hearts";
-			break;
-			case 'D': suit = "Diamonds";
-			break;
+			if(ch3 == 'S' || ch3 == 'C' || ch3 == 'H' || ch3 == 'D') {
+				switch(ch3) {
+				case 'S': suit = "Spades";
+				break;
+				case 'C': suit = "Clubs";
+				break;
+				case 'H': suit = "Hearts";
+				break;
+				case 'D': suit = "Diamonds";
+				break;
+				}
 			}
+			else return "Unknown";
 		}
 		else {
 			return "Unknown";
