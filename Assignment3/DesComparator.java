0ca0fa373883b5fbd9ce5d1089cc8d0814df5cb6
ryamada1015 <60910478+ryamada1015@users.noreply.github.com>
2021-07-19 17:43:08 -0700

    Add files via upload

--- Assignment3/DesComparator.java
+++ Assignment3/DesComparator.java
@@ -8,12 +8,12 @@ public class DesComparator implements Comparator<Appointment> {
 	public int compare(Appointment a1, Appointment a2) {
 		if(a1.getDescription().compareTo(a2.getDescription()) < 0)
 			return -1;
-		else if(a1.getStartDate().isBefore(a2.getStartDate()))
-			return -1;
-		else if(a1.getStartDate().equals(a2.getStartDate()) && a1.getEndDate().isBefore(a2.getEndDate()))
-			return -1;
-		else if(a1.getStartDate().equals(a2.getStartDate()) && a1.getEndDate().equals(a2.getEndDate()))
-			return -1;
+//		else if(a1.getStartDate().isBefore(a2.getStartDate()))
+//			return -1;
+//		else if(a1.getStartDate().equals(a2.getStartDate()) && a1.getEndDate().isBefore(a2.getEndDate()))
+//			return -1;
+//		else if(a1.getStartDate().equals(a2.getStartDate()) && a1.getEndDate().equals(a2.getEndDate()))
+//			return -1;
 		return 1;
 	}
 }
